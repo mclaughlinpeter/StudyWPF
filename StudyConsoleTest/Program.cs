1567f4f1ca2bd3c57392368d15bdab1512c87269
McLPeter <p.f.mclaughlin@hotmail.co.uk>
2017-08-26 09:35:30 -0400

    Add test-drive console app

---
+++ StudyConsoleTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudyDAL.Repos;
+using StudyDAL.Models;
+using StudyDAL.EF;
+using System.Data.Entity;
+
+namespace StudyConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database.SetInitializer(new DataInitializer());
+            PrintAllEntries();
+        }
+
+        static void PrintAllEntries()
+        {
+            using (var repo = new EntryRepo())
+            {
+                foreach (Entry e in repo.GetAll())
+                {
+                    Console.WriteLine($"Entry ID: {e.EntryID}\tSubject: {e.Subject}\tDuration: {e.Duration}");
+                }
+            }
+        }
+    }
+}
