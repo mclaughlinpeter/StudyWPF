994a8370273cfcbc0728b2c2001a06d839cbefc5
McLPeter <p.f.mclaughlin@hotmail.co.uk>
2017-08-26 08:18:49 -0400

    Add DbSet<Entry> table to context

--- StudyDAL/EF/StudyEntities.cs
+++ StudyDAL/EF/StudyEntities.cs
@@ -3,28 +3,14 @@ namespace StudyDAL.EF
     using System;
     using System.Data.Entity;
     using System.Linq;
+    using StudyDAL.Models;
 
     public class StudyEntities : DbContext
     {
-        // Your context has been configured to use a 'StudyEntities' connection string from your application's 
-        // configuration file (App.config or Web.config). By default, this connection string targets the 
-        // 'StudyDAL.EF.StudyEntities' database on your LocalDb instance. 
-        // 
-        // If you wish to target a different database and/or database provider, modify the 'StudyEntities' 
-        // connection string in the application configuration file.
         public StudyEntities() : base("name=StudyConnection")
         {
         }
 
-        // Add a DbSet for each entity type that you want to include in your model. For more information 
-        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
-
-        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+        public virtual DbSet<Entry> Entries { get; set; }
     }
-
-    //public class MyEntity
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //}
 }
