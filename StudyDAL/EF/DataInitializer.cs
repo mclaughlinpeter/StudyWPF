6fd77ef8e5ed25ae3e175cb445940ee530af4e00
McLPeter <p.f.mclaughlin@hotmail.co.uk>
2017-08-26 09:23:19 -0400

    Add DataInitializer

---
+++ StudyDAL/EF/DataInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using StudyDAL.Models;
+
+namespace StudyDAL.EF
+{
+    public class DataInitializer : DropCreateDatabaseAlways<StudyEntities>
+    {
+        protected override void Seed(StudyEntities context)
+        {
+            var entries = new List<Entry>
+            {
+                new Entry {Subject = "C#", Duration = 30 },
+                new Entry {Subject = "JS", Duration = 60 },
+                new Entry {Subject = "C++", Duration = 45 },
+                new Entry {Subject = "Linux", Duration = 75 },
+                new Entry {Subject = "C#", Duration = 30 }
+            };
+            entries.ForEach(x => context.Entries.Add(x));
+            context.SaveChanges();
+        }
+    }
+}
