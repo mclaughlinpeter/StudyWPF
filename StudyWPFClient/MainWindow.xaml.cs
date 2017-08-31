6a2f8670b37d6ce64973cdcef2fd03171363a5ef
McLPeter <p.f.mclaughlin@hotmail.co.uk>
2017-08-31 09:13:50 -0400

    Add HashSet<string> to WPF client to present only unique subject values in the subject combo box

--- StudyWPFClient/MainWindow.xaml.cs
+++ StudyWPFClient/MainWindow.xaml.cs
@@ -30,9 +30,11 @@ namespace StudyWPFClient
                 new Entry { EntryID = 1, Subject = "C#", Duration = new TimeSpan(2, 30, 0), DateTimeStamp = DateTime.Now.AddHours(1) },
                 new Entry { EntryID = 2, Subject = "JS", Duration = new TimeSpan(0, 45, 0), DateTimeStamp = DateTime.Now.AddMinutes(30) },
                 new Entry { EntryID = 3, Subject = "C++", Duration = new TimeSpan(1, 15, 0), DateTimeStamp = DateTime.Now.AddDays(2) },
-                new Entry { EntryID = 4, Subject = "Linux", Duration = new TimeSpan(0, 30, 0), DateTimeStamp = DateTime.Now.AddMonths(1) }
+                new Entry { EntryID = 4, Subject = "Linux", Duration = new TimeSpan(0, 30, 0), DateTimeStamp = DateTime.Now.AddMonths(1) },
+                new Entry { EntryID = 5, Subject = "C++", Duration = new TimeSpan(1, 30, 0), DateTimeStamp = DateTime.Now.AddDays(3) },
+                new Entry { EntryID = 6, Subject = "Linux", Duration = new TimeSpan(0, 45, 0), DateTimeStamp = DateTime.Now.AddMonths(2) }
             };
-            studySubjects.ItemsSource = _entries;
+            studySubjects.ItemsSource = new HashSet<string>(from e in _entries select e.Subject);
         }
     }
 }
