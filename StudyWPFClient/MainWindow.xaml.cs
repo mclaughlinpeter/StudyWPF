7b71c4688a9d438c624733658952dc7035b2be4b
McLPeter <p.f.mclaughlin@hotmail.co.uk>
2017-08-31 08:25:51 -0400

    Add Subject combo box and test with dummy data

--- StudyWPFClient/MainWindow.xaml.cs
+++ StudyWPFClient/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using StudyDAL.Models;
 
 namespace StudyWPFClient
 {
@@ -20,9 +21,18 @@ namespace StudyWPFClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly IList<Entry> _entries;
         public MainWindow()
         {
             InitializeComponent();
+            _entries = new List<Entry>
+            {
+                new Entry { EntryID = 1, Subject = "C#", Duration = new TimeSpan(2, 30, 0), DateTimeStamp = DateTime.Now.AddHours(1) },
+                new Entry { EntryID = 2, Subject = "JS", Duration = new TimeSpan(0, 45, 0), DateTimeStamp = DateTime.Now.AddMinutes(30) },
+                new Entry { EntryID = 3, Subject = "C++", Duration = new TimeSpan(1, 15, 0), DateTimeStamp = DateTime.Now.AddDays(2) },
+                new Entry { EntryID = 4, Subject = "Linux", Duration = new TimeSpan(0, 30, 0), DateTimeStamp = DateTime.Now.AddMonths(1) }
+            };
+            studySubjects.ItemsSource = _entries;
         }
     }
 }
