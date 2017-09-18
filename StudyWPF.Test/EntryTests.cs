9d9b7600a4003cb7ea1538a3aa61c6e00f3bb463
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-09-18 19:58:36 -0400

    Add unit test to test Entry.ToString() method

---
+++ StudyWPF.Test/EntryTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudyDAL.Models;
+
+namespace StudyWPF.Test
+{
+    [TestClass]
+    public class EntryTests
+    {
+        [TestMethod]
+        public void Entry_Returns_String()
+        {
+            //  Arrange
+            Entry entry = new Entry() { Subject = "C#", Duration = new TimeSpan(2, 30, 0), DateTimeStamp = DateTime.Now.AddHours(1) };
+            string expected = $"Id: {entry.EntryID}\tSubject: {entry.Subject}\tDuration: {entry.Duration.ToString()}\tDTS: {entry.DateTimeStamp.ToString()}";
+
+            //  Act
+            string result = entry.ToString();
+
+            //  Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
