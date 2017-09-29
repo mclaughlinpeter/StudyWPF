8a920bb6e469b6ba3b74fa5c6b9f12c12bd330b2
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-09-29 17:45:23 -0400

    Add NewEntry.cs class with INotifyPropertyChanged implemented for the NewSubject property

---
+++ StudyWPFClient/ViewModels/NewEntry.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyWPFClient.ViewModels
+{
+    public class NewEntry : INotifyPropertyChanged
+    {
+        private readonly IList<string> uniSub;
+
+        public NewEntry(IList<string> uniqueSubjects)
+        {
+            uniSub = uniqueSubjects;
+        }
+
+        public DateTime DateTimeStamp { get; set; }
+
+        private string _subject;
+        public string Subject
+        {
+            get { return _subject; }
+            set
+            {
+                if (value == _subject)
+                    return;
+                _subject = value;
+                OnPropertyChanged(nameof(Subject));
+            }
+        }
+
+        public TimeSpan Duration { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
