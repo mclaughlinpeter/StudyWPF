1bea2ef15a248a63fa1238b1a1292ba8a07c16d5
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-12-10 10:09:11 -0500

    Add read and write methods to StudyDat class and add study_test.dat test file to StudyDAL project folder with ignore in .gitignore

--- StudyDAL/Dat/StudyDat.cs
+++ StudyDAL/Dat/StudyDat.cs
@@ -2,11 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace StudyDAL.Dat
 {
     public class StudyDat
     {
+        private static string path = @"C:\Users\Peter\OneDrive - Slurp\ABI\Projects\StudyWPF\StudyDAL\study_test.dat";
+
+        public static void WriteText(string value)
+        {
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(value + " " + (i + 1));
+                }
+            }
+        }
+
+        public static IList<string> ReadText()
+        {
+            List<string> lines = null;
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string line;
+                    lines = new List<string>();
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        lines.Add(line);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return lines;
+        }
     }
 }
