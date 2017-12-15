e567dadb6c75c9535e12e717e2506e28ca51b299
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-12-15 08:04:48 -0500

    Add initial StudyTimer class, implementing ITimer and encapsulating a System.Threading.Timer object

--- StudyWPFClient/ViewModels/StudyTimer.cs
+++ StudyWPFClient/ViewModels/StudyTimer.cs
@@ -2,11 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace StudyWPFClient.ViewModels
 {
-    public class StudyTimer
+    public class StudyTimer : ITimer
     {
+        private Timer _timer;
+        private bool _timerRunning;
+
+        public StudyTimer(TimerCallback callback, int period)
+        {
+            _timer = new Timer(callback, null, Timeout.Infinite, period);
+
+            _timerRunning = false;
+        }
+        
+        public void Start()
+        {
+            _timer.Change(1000, 1000);
+            _timerRunning = true;
+        }
+
+        public void Stop()
+        {
+            _timer.Change(Timeout.Infinite, Timeout.Infinite);
+            _timerRunning = false;
+        }
+
+        public void Toggle()
+        {
+            if (_timerRunning)
+                this.Stop();
+            else
+                this.Start();
+        }
     }
 }
