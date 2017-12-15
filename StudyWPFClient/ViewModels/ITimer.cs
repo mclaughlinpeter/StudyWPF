269506033ee0ea6264b1440a536459d79299a60f
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-12-15 07:48:15 -0500

    Add initial ITimer interface to ViewModels folder

---
+++ StudyWPFClient/ViewModels/ITimer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyWPFClient.ViewModels
+{
+    interface ITimer
+    {
+        void Reset();
+
+        void Toggle();
+
+        void Start();
+
+        void Stop();
+    }
+}
